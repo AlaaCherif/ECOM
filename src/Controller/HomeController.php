cf1152013acf3f94e3375b4744b2bea843fcc6ee
Alaa <alaaeddinecherif@insat.u-carthage.tn>
2021-07-11 23:24:11 +0100

    holy shit thats so much stuff (auth)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,55 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
-use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Sodium\add;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(EntityManagerInterface $em,Request $request): Response
+    #[Route('/home', name: 'home')]
+    public function index(): Response
     {
-        $form=$this->createFormBuilder(null)
-            ->add('Search',TextType::class,[
-                'attr'=>[
-                    'class'=>'form-control me-sm-2',
-                    'placeholder'=>'Search Procuts',
-                    'type'=>'text'
-                ],
-                'label'=>false
-            ])
-            ->add('submit',SubmitType::class,[
-                'attr'=>[
-                    'class'=>'btn btn-secondary my-2 my-sm-0',
-                    'float'=>'right'
-                    
-                ]
-            ])
-            ->getForm();
-        $form->handleRequest($request);
-        $repo=$this->getDoctrine()->getRepository(Product::class);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $keyword=$form->getData();
-            $products=$repo->findBy(['name'=>$keyword]);
-            return $this->render('home.html.twig',[
-                'prods'=>$products,
-                'form'=>$form->createView()
-            ]);
-        }
-
-        $products=$repo->findBy(['recommended'=>'true']);
-        return $this->render('home.html.twig',[
-            'prods'=>$products,
-            'form'=>$form->createView()
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,18 +10,60 @@ use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationRequestHandler
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
+use App\Entity\Product;
 
 class LoginController extends AbstractController
 {
     private $session;
+    #[Route('/', name: 'home')]
+    public function home(EntityManagerInterface $em,Request $request): Response
+    {
+        $form=$this->createFormBuilder(null)
+            ->add('Search',TypeTextType::class,[
+                'attr'=>[
+                    'class'=>'form-control me-sm-2',
+                    'placeholder'=>'Search Procuts',
+                    'type'=>'text'
+                ],
+                'label'=>false
+            ])
+            ->add('submit',SubmitType::class,[
+                'attr'=>[
+                    'class'=>'btn btn-secondary my-2 my-sm-0',
+                    'float'=>'right'
+                    
+                ]
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        $repo=$this->getDoctrine()->getRepository(Product::class);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $keyword=$form->getData();
+            $products=$repo->findBy(['name'=>$keyword]);
+            return $this->render('home.html.twig',[
+                'prods'=>$products,
+                'form'=>$form->createView()
+            ]);
+        }
+
+        $products=$repo->findBy(['recommended'=>'true']);
+        return $this->render('home.html.twig',[
+            'prods'=>$products,
+            'form'=>$form->createView()
+        ]);
+    }
     public function __construct()
     {
         $this->session =new Session();
     }
-    #[Route('/login', name: 'login')]
-    public function index(HttpFoundationRequest $request,EntityManagerInterface $em): Response
+    #[Route('/signup', name: 'singup')]
+    public function index(Request $request,EntityManagerInterface $em): Response
     {
         $user=new User();
         $form=$this->createForm(SignUpType::class,$user);
@@ -32,11 +74,29 @@ class LoginController extends AbstractController
             $em->persist($user);
             $em->flush();
             $this->addFlash('success', 'Account created');
-            return $this->redirectToRoute('home');
+            $this->session->start();
+            $this->session->set('user',$user);
+            return $this->render('base.html.twig',[
+                
+            ]);
         }
         $view=$form->createView();
         return $this->render('login/index.html.twig', [
             'form' =>$view,
         ]);
+    }
+    #[Route('/first', name:'first')]
+    public function  first(Request $request)
+    {
+        if($this->session->get('id') !== -1 && $this->session->get('id') !==null )
+        {
+            $this->redirectToRoute('home');
+        }
+        else
+        {
+            return $this->render('login/index.html.twig',[
+            'username'=>$this->session->get('id')
+        ]);
+    }
     }
 }
